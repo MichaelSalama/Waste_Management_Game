e430667cb426416390059394c80290417266b439
MichaelSalama <michael.fawzy91@yahoo.com>
2018-12-30 23:13:29 +0200

    Scene Rebuild with sprites & prefabs added

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraManager : MonoBehaviour {
+
+    public float horizontalResolution = 1920;
+
+    private void OnGUI()
+    {
+        float currentAspect = (float)Screen.width / (float) Screen.height;
+        Camera.main.orthographicSize = horizontalResolution / currentAspect / 200;
+    }
+
+}
