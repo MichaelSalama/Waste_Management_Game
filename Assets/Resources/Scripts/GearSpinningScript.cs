fd93f61ef15c1c1843be4d2ab0b092d5301cbaef
MichaelSalama <michael.fawzy91@yahoo.com>
2018-12-31 14:01:35 +0200

    Scene sort fix & Gears Animattion

---
+++ Assets/Resources/Scripts/GearSpinningScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearSpinningScript : MonoBehaviour
+{
+    public float gearSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, gearSpeed) * Time.deltaTime * 20);
+    }
+}
