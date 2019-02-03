b3fab675a8cdd2123b1452eb66c4c6fd73aa36cd
MichaelSalama <michael.fawzy91@yahoo.com>
2019-02-03 18:17:57 +0200

    flow manager is back

--- Assets/Resources/Development/Scripts/ObjectsManager.cs
+++ Assets/Resources/Development/Scripts/ObjectsManager.cs
@@ -14,8 +14,6 @@ public class ObjectsManager : MonoBehaviour
 
     public List<GameObject> lights = new List<GameObject>();
 
-
-
     int randNo = 0;
     int rand = 0;
 
@@ -33,39 +31,34 @@ public class ObjectsManager : MonoBehaviour
             previousObj = ObjectPool.Spawn(glassList[1], transform.position, Quaternion.identity);
         }
 
-
         if (randNo == 1 && previousObj.GetComponent<DragHandler>().type != ObjectTypeEnum.Glass)
         {
             rand = Random.Range(1, glassList.Count);
             currentObj = ObjectPool.Spawn(glassList[rand], transform.position, Quaternion.identity);
-            //greenLight.gameObject.SetActive(true);
             LightsHandler();
-            //FlowManager.Instance.currentType = ObjectTypeEnum.Glass;
+            FlowManager.Instance.currentType = ObjectTypeEnum.Glass;
 
         }
         else if (randNo == 2 && previousObj.GetComponent<DragHandler>().type != ObjectTypeEnum.Metal)
         {
             rand = Random.Range(1, metalList.Count);
             currentObj = ObjectPool.Spawn(metalList[rand], transform.position, Quaternion.identity);
-            //redLight.gameObject.SetActive(true);
             LightsHandler();
-            //FlowManager.Instance.currentType = ObjectTypeEnum.Metal;
+            FlowManager.Instance.currentType = ObjectTypeEnum.Metal;
         }
         if (randNo == 3 && previousObj.GetComponent<DragHandler>().type != ObjectTypeEnum.Paper)
         {
             rand = Random.Range(1, paperList.Count);
             currentObj = ObjectPool.Spawn(paperList[rand], transform.position, Quaternion.identity);
-            //yellowLight.gameObject.SetActive(true);
             LightsHandler();
-            //FlowManager.Instance.currentType = ObjectTypeEnum.Paper;
+            FlowManager.Instance.currentType = ObjectTypeEnum.Paper;
         }
         if (randNo == 4 && previousObj.GetComponent<DragHandler>().type != ObjectTypeEnum.Plastic)
         {
             rand = Random.Range(1, plasticList.Count);
             currentObj = ObjectPool.Spawn(plasticList[rand], transform.position, Quaternion.identity);
-            //blueLight.gameObject.SetActive(true);
             LightsHandler();
-            //FlowManager.Instance.currentType = ObjectTypeEnum.Plastic;
+            FlowManager.Instance.currentType = ObjectTypeEnum.Plastic;
         }
         return currentObj;
     }
@@ -81,8 +74,6 @@ public class ObjectsManager : MonoBehaviour
             }
         }
     }
-
-   
 
     IEnumerator WaitforTime()
     {
